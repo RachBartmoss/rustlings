830ba6d6ce946ccc434de729f4f2b869bef95a5f
RachBartmoss <julesleveillard@gmail.com>
2024-02-06 22:37:20 +0100

    Done until iterators part

--- exercises/16_lifetimes/lifetimes3.rs
+++ exercises/16_lifetimes/lifetimes3.rs
@@ -5,17 +5,33 @@
 // Execute `rustlings hint lifetimes3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-struct Book {
-    author: &str,
-    title: &str,
+
+// In this case the Book struct holds to string slice, which are both references. The Compiler
+// must be sure that the 2 reference contained in this struct are not poiting to undefined or 
+// undesirable data (what rust calls a dangling reference). To accomplish this we add the generic
+// lifetime a' in our struct declaration and apply it to both reference. what we are doing here
+// is telling the compiler "A Book struct cannot have a longer lifetime than any of the 2 reference
+// it contains
+
+struct Book <'a> {
+    author: &'a str,
+    title: &'a str,
 }
+
+
 
 fn main() {
     let name = String::from("Jill Smith");
+    
+    
     let title = String::from("Fish Flying");
+    
+    
+
     let book = Book { author: &name, title: &title };
+
+    
 
     println!("{} by {}", book.title, book.author);
 }
--- exercises/17_tests/tests4.rs
+++ exercises/17_tests/tests4.rs
@@ -5,7 +5,19 @@
 // Execute `rustlings hint tests4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
+
+// here the first test function simply verify that the rectangle Constructor, given a width : x
+// and a heigth : y , truly returns a  rectangle struct with width x and heigth y, so we need
+// to check for both fields of the struct during our test.
+//
+// For the second and third test, we must first notice that the constructor function uses the
+// panic!() macro if width OR heigth is negative (inferior to 0), this macro terminate the 
+// program and to make a test that checks if the function makes the programm panic, we must
+// use the [should_panic] attribute. We can even be more specific and feed the attribute a String
+// with the expected keyword so that should_panic checks for this String in the Panic!() message and
+// only validate if both message are the same, this could be useful if we want to confirm that the panic
+// we catch is truly the panic we intended to catch.
 
 struct Rectangle {
     width: i32,
@@ -30,17 +42,19 @@ mod tests {
     fn correct_width_and_height() {
         // This test should check if the rectangle is the size that we pass into its constructor
         let rect = Rectangle::new(10, 20);
-        assert_eq!(???, 10); // check width
-        assert_eq!(???, 20); // check height
+        assert_eq!(rect.width , 10); // check width
+        assert_eq!(rect.height , 20); // check height
     }
 
     #[test]
+    #[should_panic]
     fn negative_width() {
         // This test should check if program panics when we try to create rectangle with negative width
         let _rect = Rectangle::new(-10, 10);
     }
 
     #[test]
+    #[should_panic(expected = "Rectangle width and height cannot be negative!")]
     fn negative_height() {
         // This test should check if program panics when we try to create rectangle with negative height
         let _rect = Rectangle::new(10, -10);
